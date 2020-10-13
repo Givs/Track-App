7cc55799ffe81abed3a698c520898dcbe2c682ba
Givaldo Neto <valdingivs@outlook.com>
2020-10-13 00:06:22 -0300

    Auth

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,7 +1,9 @@
+require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
@@ -21,8 +23,8 @@ mongoose.connection.on('error', (err) => {
     console.log('Error connecting to mongo', err);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hi there');
+app.get('/', requireAuth ,(req, res) => {
+    res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const User = mongoose.model('User');
 
 const router = express.Router();
 
-router.post('/signup', (req, res) => {
-    console.log(req.body);
-    res.send('You made a post request');
+router.post('/signup', async (req, res) => {
+    const { email, password } = req.body;
+    
+    try{
+        const user = new User({ email, password });
+        await user.save();
+
+        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY')
+        res.send({ token });
+    } catch(err){
+        return res.status(422).send(err.message);
+    }
+    
 });
 
 module.exports = router;
