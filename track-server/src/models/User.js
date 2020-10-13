1e2298157c18ea17fe6e40147e9e3af396970dd0
Givaldo Neto <valdingivs@outlook.com>
2020-10-13 01:05:12 -0300

    salting and hashing

--- track-server/src/models/User.js
+++ track-server/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -11,5 +12,28 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
+
+userSchema.pre('save', function (next){
+    const user = this;
+    if ( !user.isModified('password')) {
+        return next();
+    }
+
+    bcrypt.genSalt(10, (err, salt) => {
+        if (err) {
+            return next(err);
+        }
+
+        bcrypt.hash(user.password, salt, (err, hash) => {
+            if (err) {
+                return next(err);
+            }
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+
 
 mongoose.model('User', userSchema);
