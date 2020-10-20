52076903741f498cd8be84c7033fa5c87e267c6e
Givaldo Neto <valdingivs@outlook.com>
2020-10-19 23:27:54 -0300

    Input props and show/hide button

--- frontend/src/screens/SignupScreen.js
+++ frontend/src/screens/SignupScreen.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const SignupScreen = ({ navigation }) => {
+
+    const [showEye, setShowEye] = useState(false);
+    const [visible, setVisible] = useState(true);
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
   return (
-    <>
+    <View style={styles.container}>
       <Spacer>
         <Text h3>Sign Up for Tracker</Text>
       </Spacer>
-      <Input label="Email" />
+      <Input 
+        label="Email"
+        placeholder="example@example.com" 
+        value={email} 
+        onChangeText={(newEmail) => setEmail(newEmail)}
+        autoCapitalize="none"
+        autoCorrect={false}
+       />
       <Spacer />
-      <Input label="Password" />
+      <View>
+        <Input 
+            secureTextEntry={visible}
+            label="Password" 
+            placeholder="******"
+            value={password}
+            onChangeText={(newPassword) => setPassword(newPassword)}
+            autoCapitalize="none"
+            autoCorrect={false}
+        />
+        <TouchableOpacity style={styles.bntEye} onPress={
+            () => {
+                setVisible(!visible)
+                setShowEye(!showEye)
+            }
+        }>
+            <MaterialCommunityIcons 
+                name={showEye === false ? 'eye-outline' : 'eye-off-outline'}
+                size={26}
+                color={'black'}
+            />
+        </TouchableOpacity>
+      </View>
       <Spacer>
         <Button title="Sign Up" />
       </Spacer>
-    </>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        marginBottom: 200
+    },
+    bntEye: {
+        position: 'absolute',
+        right:25,
+        top: 8
+    }
+});
 
 export default SignupScreen;
